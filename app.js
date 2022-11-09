527a75a149b48cc030d72d7b0aaae8a595cbfd1b
xxaydiv <vthanvanthriu@hubspot.com>
2022-11-09 18:33:03 +0800

    change package json32

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
-app.listen(3000, function()
-  {console.log("Server started on port 3000");}
-);
 
 app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
   res.render('home');
 });
+app.listen(3000, function()
+  {console.log("Server started on port 3000");}
+);
