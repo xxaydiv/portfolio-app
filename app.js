aca5fd7034b2efd77b05357c279a65138613ec34
xxaydiv <vthanvanthriu@hubspot.com>
2022-11-11 17:38:57 +0800

    aboutpage2

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
   res.render('home');
 });
+app.get("/about", function(req, res){
+  res.render('about');
+});
+
 app.listen(3000, function()
   {console.log("Server started on port 3000");}
 );
